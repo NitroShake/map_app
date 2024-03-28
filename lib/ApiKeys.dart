0eb2863e61687429979ae3ae79e9b32395ac0fe5
NitroShake <63812175+NitroShake@users.noreply.github.com>
2024-03-28 16:04:21 +0000

    Create ApiKeys.dart

---
+++ lib/ApiKeys.dart
@@ -0,0 +1,12 @@
+//You can't have them. Sorry :/
+class ApiKeys {
+  final String? tripAdvisorKey = null;
+
+  ApiKeys._privateConstructor();
+
+  static final ApiKeys _instance = ApiKeys._privateConstructor();
+
+  factory ApiKeys() {
+    return _instance;
+  }
+}
