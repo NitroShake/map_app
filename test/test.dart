4af07873fae7444ac689eb4f4f9cb8cd1c777a91
NitroShake <63812175+NitroShake@users.noreply.github.com>
2024-04-07 17:29:12 +0100

    unit test template

--- test/test.dart
+++ test/test.dart
@@ -9,13 +9,14 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:map_app/MapRoute.dart';
 
 import 'package:map_app/main.dart';
 
 class _HttpOverrides extends HttpOverrides {}
 
-void main() {
+void main() async {
   // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
   //   // Build our app and trigger a frame.
   //   //await tester.pumpWidget(const MyApp());
@@ -33,9 +34,41 @@ void main() {
   //   expect(find.text('1'), findsOneWidget);
   // });
 
-  test("Routes parse correctly", () async {
+  MapRoute? route;
+  test("Route init", () async {
     HttpOverrides.global = _HttpOverrides();
-    MapRoute? route = await MapRoute.createNewRoute(50.770941, -0.870648, 50.844770, -0.775550, "car", "test");
+    route = await MapRoute.createNewRoute(50.770941, -0.870648, 50.844770, -0.775550, "car", "test");
+    //verify route information
+    print(route!.checkpoints[0].position);
+    expect(route!.checkpoints[0].position, LatLng(50.770858, -0.870672));
     expect(route!.checkpoints[9].locationName, "Southgate");
+  });
+
+
+  test("Route update", () async {
+    //verify route updates correctly
+    route!.update( LatLng(50.775424, -0.867573));
+    expect(route!.checkpoints[0].position, LatLng(50.769986, -0.870364));
+  });
+
+
+  test("Get location details", () async {
+
+  });
+
+  test("Get TripAdvisor details", () => {
+
+  });
+
+  test("UI - Search and load location", () => {
+
+  });
+
+  test("UI - Load and view Route", () => {
+
+  });
+
+  test("UI - Access Bookmark", () => {
+
   });
 }
